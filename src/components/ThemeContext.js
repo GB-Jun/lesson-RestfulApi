f0a1d5cfcf2dfe0ca7ee818451a34608ff722e35
GB-Jun <windgb369@hotmail.com>
2022-07-21 12:07:28 +0800

    useContext

---
+++ src/components/ThemeContext.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const themes = {
+    dark: {
+        name: 'dark',
+        bgc: 'black',
+        fc: 'white',
+    },
+    light: {
+        name: 'light',
+        bgc: 'white',
+        fc: 'black',
+    },
+    blue: {
+        name: 'blue',
+        bgc: 'blue',
+        fc: 'yellow',
+    }
+
+
+}
+
+
+
+
+const themeContext = React.createContext(themes.dark);
+
+export default themeContext;
